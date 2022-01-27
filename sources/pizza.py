a203ac7f9560a12168ed996377e9bf72339389af
Philippe ARNAL <philiparnal@gmail.com>
2022-01-27 12:56:49 +0100

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'ma pizza': {
+        'ingredients': {'mozzarella': 5, 'onion': 8},
+        'price': 12,
     }
 }
